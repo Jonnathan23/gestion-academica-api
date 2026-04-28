cbb34651267d0093d683740ced57c0009d2021a4
Jonnathan23 <jsaquicela23@gmail.com>
2026-04-28 13:18:08 -0500

    cambio en el hash

--- src/core/utils/hash.ts
+++ src/core/utils/hash.ts
@@ -1,3 +1,11 @@
 // hash.ts
-Bun.password.hash("Admin123!", { algorithm: "bcrypt", cost: 10 })
+const password = Bun.argv[2];
+
+if (!password) {
+    console.error("Por favor, proporciona una contraseña como argumento.");
+    console.log("Uso: bun run src/core/utils/hash.ts <password>");
+    process.exit(1);
+}
+
+Bun.password.hash(password, { algorithm: "bcrypt", cost: 10 })
     .then(hash => console.log("Tu hash perfecto es:", hash));
